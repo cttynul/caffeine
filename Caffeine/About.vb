86813498d72b29edcc7ead11762d2da86b7319a7
Alessandro Celso <cttynul@outlook.com>
2016-03-29 23:38:38 +0200

    first update

---
+++ Caffeine/About.vb
@@ -0,0 +1,6 @@
+﻿Public Class About
+    Private Sub LinkLicense_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLicense.LinkClicked
+        Dim url As String = "http://www.gnu.org/licenses/gpl.html"
+        Process.Start(url)
+    End Sub
+End Class
