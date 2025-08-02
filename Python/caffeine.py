ec396df43d8a5ee31802561fc29eb4bf550c0ce1
cttynul <cttynul@outlook.com>
2025-08-02 18:03:58 +0200

    +input -t

--- Python/caffeine.py
+++ Python/caffeine.py
@@ -1,25 +1,42 @@
-import win32com.client, time
+import win32com.client
+import time
+import argparse
 
 def print_logo(timeout=0):
     logo = '''
-                 ___  ___      _             
-                / __)/ __)    (_)            
-      ____ ____| |__| |__ ____ _ ____   ____ 
-     / ___) _  |  __)  __) _  ) |  _ \ / _  )
-    ( (__( ( | | |  | | ( (/ /| | | | ( (/ / 
-     \____)_||_|_|  |_|  \____)_|_| |_|\____)
-                                    - cttynul'''
+                    ___  ___       _
+                   / __)/ __)
+     ____ ____| |__| |__ ____ _ ____    ____
+    / ___) _  |  __)  __) _  ) |  _ \ / _  )
+   ( (__( ( | | |  | | ( (/ /| | | | ( (/ /
+    \____)_||_|_|  |_|  \____)_|_| |_|\____)
+                                        - cttynul'''
     print(logo)
-    if(timeout != 0): print("   Timeout set to " + str(timeout) + " seconds")
+    if timeout != 0:
+        print("    Timeout set to " + str(timeout) + " seconds")
 
 
 def main():
-    timeout=60
+    parser = argparse.ArgumentParser(description="Caffeine for Windows in a pythonic way")
+    
+    parser.add_argument(
+        '-t', '--time',
+        type=int,
+        default=60,
+        help='Timeout in seconds (default: 60)'
+    )
+    
+    args = parser.parse_args()
+    timeout = args.time
+    
+    if timeout <= 0:
+        timeout = 60
+
     print_logo(timeout)
-    while(1):
+    while True:
         shell = win32com.client.Dispatch("WScript.Shell")
         shell.SendKeys('{F15}')
         time.sleep(timeout)
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
